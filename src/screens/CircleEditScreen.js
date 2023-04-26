bbe99cae5cb4f8ed3db061a8c6993c164e01851d
PGD <rudeh1253@gmail.com>
2023-04-26 11:39:52 +0900

    Designed CircleEditScreen

--- src/screens/CircleEditScreen.js
+++ src/screens/CircleEditScreen.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
-import { Alert, Button, FlatList, Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { DEFAULT_IMAGE } from '../data/constants';
+import { Alert, Button, FlatList, Image, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { DEFAULT_IMAGE, MAIN_BLUE_COLOR, MAIN_GRAY_COLOR, MAIN_RED_COLOR, PRIORITY_COLOR_SQUARE_WIDTH_AND_SIZE, SUB_GRAY_COLOR } from '../data/constants';
 import { RadioButton } from 'react-native-paper';
 import { getPrioritySet } from '../backend/function/function';
 import { getWorkData } from '../backend/controller/workDataController';
@@ -34,35 +34,53 @@ function CircleEditScreen({ route, navigation }) {
 
   return (
     <SafeAreaView style={ styles.container }>
-      {/* <Header /> */}
-      <Text style={ styles.label }>서클 위치</Text>
-      <Text style={ styles.text }>{ dataFromPrevious.space }</Text>
-      <Text style={ styles.label }>펜네임</Text>
-      <Text style={ styles.text }>{ dataFromPrevious.penname }</Text>
-      <Text style={ styles.label }>서클명</Text>
-      <Text style={ styles.text }>{ dataFromPrevious.circle_name }</Text>
-      <View style={ styles.radioContainer }>
-        {
-          prioritySet.map((item) => {
-            return (
-              <RadioBtn key={ item.priority } item={ item } checker={ [ checked, setChecked ] }
-                  current={ dataFromPrevious } />
-            );
-          })
-        }
+      <View style={ styles.infoConatiner }>
+        <View style={ styles.circleInfoContainer }>
+          <View style={ styles.circleInfoUnit }>
+            <Text style={ styles.label }>서클 위치</Text>
+            <Text style={ styles.text }>{ dataFromPrevious.space }</Text>
+          </View>
+          <View style={ styles.circleInfoUnit }>
+            <Text style={ styles.label }>펜네임</Text>
+            <Text style={ styles.text }>{ dataFromPrevious.penname }</Text>
+          </View>
+          <View style={ styles.circleInfoUnit }>
+            <Text style={ styles.label }>서클명</Text>
+            <Text style={ styles.text }>{ dataFromPrevious.circle_name }</Text>
+          </View>
+        </View>
+        <View style={ styles.radioContainer }>
+          <Text style={ styles.title }>우선순위 선택</Text>
+          {
+            prioritySet.map((item) => {
+              return (
+                <RadioBtn key={ item.priority } item={ item } checker={ [ checked, setChecked ] }
+                    current={ dataFromPrevious } />
+              );
+            })
+          }
+        </View>
       </View>
-      <Button
-          color='red'
-          title='서클 삭제'
-          onPress={() => onDelete(navigation, dataFromPrevious.circle_id, workList, setWorkList) } />
-      <Button title='작품 추가' onPress={ () => {
-            toAddWork(navigation, dataFromPrevious);
-          } } />
-      <FlatList
-          style={ styles.workContainerList }
-          data={ workList }
-          renderItem={ ({ item }) => <WorkItem circleData={ dataFromPrevious } workData={ item } colorSet={ prioritySet } navigation={ navigation } /> }
-          keyExtractor={ (item) => item.id } />
+      <TouchableOpacity
+          style={ styles.deleteCircleBtn }
+          onPress={() => onDelete(navigation, dataFromPrevious.circle_id, workList, setWorkList) }>
+        <Text style={ styles.btnTitle }>서클 삭제</Text>
+      </TouchableOpacity>
+    
+      <View style={ styles.workContainer }>
+        <TouchableOpacity
+            style={ styles.addWorkBtn }
+            onPress={ () => {
+              toAddWork(navigation, dataFromPrevious);
+            } }>
+          <Text style={ styles.btnTitle }>작품 추가</Text>
+        </TouchableOpacity>
+        <FlatList
+            data={ workList }
+            renderItem={ ({ item }) => <WorkItem circleData={ dataFromPrevious } workData={ item } colorSet={ prioritySet } navigation={ navigation } /> }
+            keyExtractor={ (item) => item.id }
+            contentContainerStyle={ styles.workList } />
+      </View>
     </SafeAreaView>
   );
 }
@@ -100,8 +118,8 @@ function onDelete(navigation, circleId, workList) {
 function RadioBtn({ item, checker, current }) {
   const [ checked, setChecked ] = checker;
   const styleObj = {
-    width: 25,
-    height: 25,
+    width: PRIORITY_COLOR_SQUARE_WIDTH_AND_SIZE,
+    height: PRIORITY_COLOR_SQUARE_WIDTH_AND_SIZE,
     backgroundColor: item.color,
   }
   return (
@@ -121,7 +139,7 @@ function RadioBtn({ item, checker, current }) {
               setChecked(item.priority);
             } } />
       <View style={ styleObj } />
-      <Text style={ styles.text }>{ item.title }</Text>
+      <Text style={ styles.radioButtonLabel }>{ item.title }</Text>
     </View>
   )
 }
@@ -130,22 +148,16 @@ function WorkItem({ circleData, workData, colorSet, navigation }) {
   const imageSrc = workData.image_path == DEFAULT_IMAGE
       ? require('../../public/null-image.png')
       : { uri: workData.image_path };
-  const styleObj = {
-    width: 25,
-    height: 25,
-    backgroundColor: colorSet[workData.priority - 1].color,
-  }
   return (
     <TouchableOpacity style={ styles.workItem }
         onPress={ () => editWork(circleData, workData, navigation) }>
       <Image
-          style={ styles.workImage }
+          style={ [ styles.workImage, { borderColor: colorSet[workData.priority - 1].color } ] }
           source={ imageSrc }
           resizeMode='contain' />
-      <View>
-        <View style={ styleObj } />
-        <Text style={ styles.text }>{ workData.title }</Text>
-        <Text style={ styles.text }>{ workData.price }</Text>
+      <View style={ styles.workInfo }>
+        <Text style={ styles.workTitle }>{ workData.title }</Text>
+        <Text style={ styles.workPrice }>{ workData.price }</Text>
       </View>
     </TouchableOpacity>
   );
@@ -159,14 +171,33 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  header: {
+  infoConatiner: {
     flexDirection: 'row',
+    gap: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 15,
+  },
+  circleInfoContainer: {
+    margin: 10,
+    gap: 3,
+    marginBottom: 20,
+  },
+  circleInfoUnit: {
+    
   },
   text: {
-    color: '#000',
+    color: `${ MAIN_GRAY_COLOR }`,
+    fontSize: 20,
   },
   label: {
-    color: '#000',
+    color: `${ SUB_GRAY_COLOR }`,
+    fontSize: 14,
+  },
+  title: {
+    color: `${ MAIN_GRAY_COLOR }`,
+    fontSize: 16,
+    marginLeft: 13,
   },
   radioContainer: {
     flexDirection: 'column',
@@ -174,17 +205,67 @@ const styles = StyleSheet.create({
   radioItem: {
     flexDirection: 'row',
     alignItems: 'center',
+    gap: 5,
+  },
+  radioButtonLabel: {
+    color: `${ MAIN_GRAY_COLOR }`,
+    fontSize: 14,
+  },
+  deleteCircleBtn: {
+    backgroundColor: `${ MAIN_RED_COLOR }`,
+    marginHorizontal: 50,
+    padding: 15,
+    paddingHorizontal: 35,
+    borderRadius: 100,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  workContainer: {
+    flex: 1,
+    borderTopWidth: 1,
+    borderColor: `${ SUB_GRAY_COLOR }`,
+    paddingTop: 30,
+    marginTop: 30,
+  },
+  addWorkBtn: {
+    backgroundColor: `${ MAIN_BLUE_COLOR }`,
+    marginHorizontal: 50,
+    marginBottom: 20,
+    padding: 15,
+    borderRadius: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  btnTitle: {
+    color: '#fff',
+    fontWeight: '600',
+    fontSize: 20,
+  },
+  workList: {
+    gap: 15,
+  },
+  workItem: {
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    marginLeft: 10,
   },
   workImage: {
     width: 100,
     height: 100,
+    borderWidth: 3,
   },
-  workItem: {
-    flexDirection: 'row',
-    alignItems: 'center',
+  workInfo: {
+    marginTop: 10,
+    marginLeft: 10,
   },
-  workContainerList: {
-    flex: 1,
+  workTitle: {
+    color: `${ MAIN_GRAY_COLOR }`,
+    fontSize: 18,
+  },
+  workPrice: {
+    color: `${ SUB_GRAY_COLOR }`,
+    fontSize: 14,
   },
 });
 
